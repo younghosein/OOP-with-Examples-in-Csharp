fcc4e03362c62862d547a4bf8f89d9adea298979
Hosein Mohamadpor <83702864+younghosein@users.noreply.github.com>
2023-05-22 02:00:25 +0330

    Add files via upload

---
+++ Moshtari_kala/Moshtari_kala/Moshtari.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moshtari_kala
+{
+    class Moshtari
+    {
+        public string name;
+        public int tedad;
+        public int qeymat;
+        public int pol;
+        public int result()
+        {
+            pol = tedad * qeymat;
+            return pol;
+        }
+    }
+}
---
+++ Moshtari_kala/Moshtari_kala/Takhfif.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moshtari_kala
+{
+    class Takhfif
+    {
+        public int off;
+        public int natije()
+        {
+            if(Moshtari.)
+        }
+    }
+}
